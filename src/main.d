d26cd543f3ece7846378532b20b425eec2084b77
Jeff Chapman <JAChapmanII@GMail.com>
2010-05-29 14:40:04 -0400

    Created some rudimentary files common to many open source projects. Created a 'hello world'-ish main.d

---
+++ src/main.d
@@ -0,0 +1,29 @@
+/* {{{
+	Copyright 2010 Jeff Chapman
+
+	This file is a part of Universum Meum
+
+	Universum Meum is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Universum Meum is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Universum Meum.  If not, see <http://www.gnu.org/licenses/>.
+
+*/// }}}
+module main;
+
+import tango.io.Stdout;
+
+int main( char[][] )
+{
+	Stdout.formatln( "Universum Meum" );
+	Stdout.formatln( "Thanks for playing!" );
+	return 0;
+}
