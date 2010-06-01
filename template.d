44b50819e4cb404b93070e4d75632f0ebcb81630
Jeff Chapman <JAChapmanII@GMail.com>
2010-05-31 21:40:59 -0400

    Added a template file/class

---
+++ template.d
@@ -0,0 +1,33 @@
+/* {{{
+	Copyright 2010 Jeff Chapman (and Nolan Brown)
+
+	This file is a part of Universum Meum
+
+	Universum Meum is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Universum Meum is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Universum Meum.  If not, see <http://www.gnu.org/licenses/>.
+
+*/// }}}
+module mODULE;
+
+class MODULE
+{
+	public:
+		this()
+		{
+		}
+
+	protected:
+
+	private:
+
+}
