b136a014a1cc6dfb681c729fa668fd0db78e50c3
Jeff Chapman II <JAChapmanII@GMail.com>
2010-06-16 02:16:34 -0400

    Maybe compiles

--- src/forces/interior/repel.cpp
+++ src/forces/interior/repel.cpp
@@ -17,38 +17,40 @@
 	along with Universum Meum.  If not, see <http://www.gnu.org/licenses/>.
 
 */// }}}
+#ifndef REPEL_CPP
+#define REPEL_CPP
+
+#include <math.h>
 
 #include "../../force.cpp"
 #include "../../particle.cpp"
 
-Logger log;
-static this()
-{
-	log = Log.lookup( "forces.repel" );
-	log.add( new AppendConsole() );
-}
+typedef void( *Force )( Particle, Particle, long double );
 
 /// Alters particle A by applying an acceleration equal to that of a force like antigravity over deltaTime
-void Repel( long double repConst )( Particle A, Particle B, ref long double deltaTime )
+template< int repConst >
+void Repel( Particle A, Particle B, long double deltaTime )
 { //{{{
-	long double xDist = B.XPosition - A.XPosition;
-	long double yDist = B.YPosition - A.YPosition;
+	long double xDist = B.XPosition() - A.XPosition();
+	long double yDist = B.YPosition() - A.YPosition();
 	long double dist2 = (xDist * xDist) + (yDist * yDist);
 	long double dist  = sqrt( dist2 );
 
-	if( ( dist < (A.Radius + B.Radius) ) && ( dist2 > 0 ) )
+	if( ( dist < (A.Radius() + B.Radius()) ) && ( dist2 > 0 ) )
 	{
-		long double repMass = repConst * B.Mass * 2.0;
-		long double sectLength = (A.Radius+B.Radius) - dist;
+		long double repMass = repConst * B.Mass() * 2.0;
+		long double sectLength = (A.Radius()+B.Radius()) - dist;
 		long double unitX = xDist / dist;
 		long double unitY = yDist / dist;
 
-		A.XAcceleration = A.NextXAcceleration -
-			( repMass ) * ( unitX ) / ( dist2 );
+		A.XAcceleration( A.NextXAcceleration() -
+			( repMass ) * ( unitX ) / ( dist2 ) );
 
-		A.YAcceleration = A.NextYAcceleration -
-			( repMass ) * ( unitY ) / ( dist2 );
+		A.YAcceleration( A.NextYAcceleration() -
+			( repMass ) * ( unitY ) / ( dist2 ) );
 	}
 } //}}}
 
-Force DefaultRepel = &Repel!( 0.0 );
+Force DefaultRepel = &Repel< 0 >;
+
+#endif // REPEL_CPP
