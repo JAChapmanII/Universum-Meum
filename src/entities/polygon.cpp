e7e3d9c48e08a5235e49d3f29d01aec2f1c575e1
Jeff Chapman II <JAChapmanII@GMail.com>
2010-06-17 20:30:46 -0400

    Split polygon, made some things public

--- src/entities/polygon.cpp
+++ src/entities/polygon.cpp
@@ -20,154 +20,117 @@
 #ifndef POLYGON_CPP
 #define POLYGON_CPP
 
-#include <math.h>
+#include "polygon.hpp"
 
-#include <GL/gl.h>
-
-#include "../entity.cpp"
-
-const long double PI = 3.14159216535898;
-const long double PI_2 = 1.5707963267949;
-
-class Polygon : public Entity
+Polygon::Polygon( long double iX, long double iY ) :
+	Entity( iX, iY, 0 ), //{{{
+	radius( 1 ),
+	numSegments( 36 )
 {
-	public:
-		Polygon( long double iX, long double iY ) :
-			Entity( iX, iY, 0 ), //{{{
-			m_Radius( 1 ),
-			m_NumSegments( 36 )
-		{
-			this->position.x = iX;
-			this->position.y = iY;
-			Color( 1.0f, 1.0f, 1.0f, 1.0f );
-		} //}}}
+	this->position.x = iX;
+	this->position.y = iY;
+	Color( 1.0f, 1.0f, 1.0f, 1.0f );
+} //}}}
 
-		Polygon( long double iX, long double iY, long double iRadius ) :
-			Entity( iX, iY, 0 ), //{{{
-			m_Radius( iRadius ),
-			m_NumSegments( 36 )
-		{
-			this->position.x = iX;
-			this->position.y = iY;
-			Color( 1.0f, 1.0f, 1.0f, 1.0f );
-		} //}}}
+Polygon::Polygon( long double iX, long double iY, long double iRadius ) :
+	Entity( iX, iY, 0 ), //{{{
+	radius( iRadius ),
+	numSegments( 36 )
+{
+	this->position.x = iX;
+	this->position.y = iY;
+	Color( 1.0f, 1.0f, 1.0f, 1.0f );
+} //}}}
 
-		Polygon( long double iX, long double iY, long double iRadius, long double iRed, long double iGreen, long double iBlue ) :
-			Entity( iX, iY, 0 ), //{{{
-			m_Radius( iRadius ),
-			m_NumSegments( 36 )
-		{
-			this->position.x = iX;
-			this->position.y = iY;
-			Color( iRed, iGreen, iBlue, 1.0f );
-		} //}}}
+Polygon::Polygon( long double iX, long double iY, long double iRadius, long double iRed, long double iGreen, long double iBlue ) :
+	Entity( iX, iY, 0 ), //{{{
+	radius( iRadius ),
+	numSegments( 36 )
+{
+	this->position.x = iX;
+	this->position.y = iY;
+	Color( iRed, iGreen, iBlue, 1.0f );
+} //}}}
 
-		Polygon( long double iX, long double iY, long double iRadius, long double iRed, long double iGreen, long double iBlue, long double iAlpha ) :
-			Entity( iX, iY, 0 ), //{{{
-			m_Radius( iRadius ),
-			m_NumSegments( 36 )
-		{
-			this->position.x = iX;
-			this->position.y = iY;
-			Color( iRed, iGreen, iBlue, iAlpha );
-		} //}}}
+Polygon::Polygon( long double iX, long double iY, long double iRadius, long double iRed, long double iGreen, long double iBlue, long double iAlpha ) :
+	Entity( iX, iY, 0 ), //{{{
+	radius( iRadius ),
+	numSegments( 36 )
+{
+	this->position.x = iX;
+	this->position.y = iY;
+	Color( iRed, iGreen, iBlue, iAlpha );
+} //}}}
 
-		void Draw( long double m_Zoom = 1 )
-		{ //{{{
-			glColor4f( Red(), Green(), Blue(), Alpha() );
-			glPolygonMode( GL_FRONT, GL_FILL );
-			glBegin( GL_POLYGON );
-				long double delTheta = 2 * PI / (long double)( NumSegments() );
-				long double theta = 0;
-				for( unsigned int segment = 0; segment < NumSegments(); ++segment )
-				{
-					theta = segment * delTheta;
-					glVertex2f( Radius() * cos( theta ) + position.x,
-								Radius() * sin( theta ) + position.y );
-				}
-			glEnd();
-		} //}}}
-
-		/// {G,S}etter for m_NumSegments
-		unsigned int NumSegments() //{{{
+void Polygon::Draw( long double zoom )
+{ //{{{
+	glColor4f( Red(), Green(), Blue(), Alpha() );
+	glPolygonMode( GL_FRONT, GL_FILL );
+	glBegin( GL_POLYGON );
+		long double delTheta = 2 * M_PI / (long double)( numSegments );
+		long double theta = 0;
+		for( unsigned int segment = 0; segment < numSegments; ++segment )
 		{
-			return m_NumSegments;
+			theta = segment * delTheta;
+			glVertex2f( radius * cos( theta ) + position.x,
+						radius * sin( theta ) + position.y );
 		}
-		void NumSegments( unsigned int nNumSegments )
-		{
-			m_NumSegments = nNumSegments;
-		} //}}}
+	glEnd();
+} //}}}
 
-		/// {G,S}etter for m_Radius
-		long double Radius() //{{{
-		{
-			return m_Radius;
-		}
-		void Radius( long double nRadius )
-		{
-			m_Radius = nRadius;
-		} //}}}
+void Polygon::Color( long double nRed, long double nGreen, long double nBlue, long double nAlpha )
+{ //{{{
+	m_Color[ 0 ] = nRed;
+	m_Color[ 1 ] = nGreen;
+	m_Color[ 2 ] = nBlue;
+	m_Color[ 3 ] = nAlpha;
+} //}}}
 
-		void Color( long double nRed, long double nGreen, long double nBlue, long double nAlpha )
-		{ //{{{
-			m_Color[ 0 ] = nRed;
-			m_Color[ 1 ] = nGreen;
-			m_Color[ 2 ] = nBlue;
-			m_Color[ 3 ] = nAlpha;
-		} //}}}
+void Polygon::Color( long double nRed, long double nGreen, long double nBlue )
+{ //{{{
+	m_Color[ 0 ] = nRed;
+	m_Color[ 1 ] = nGreen;
+	m_Color[ 2 ] = nBlue;
+} //}}}
 
-		void Color( long double nRed, long double nGreen, long double nBlue )
-		{ //{{{
-			m_Color[ 0 ] = nRed;
-			m_Color[ 1 ] = nGreen;
-			m_Color[ 2 ] = nBlue;
-		} //}}}
+// {G,S} for m_Color[ 0 ]
+long double Polygon::Red() //{{{
+{
+	return m_Color[ 0 ];
+}
+void Polygon::Red( long double nRed )
+{
+	m_Color[ 0 ] = nRed;
+} //}}}
 
-		// {G,S} for m_Color[ 0 ]
-		long double Red() //{{{
-		{
-			return m_Color[ 0 ];
-		}
-		void Red( long double nRed )
-		{
-			m_Color[ 0 ] = nRed;
-		} //}}}
+// {G,S} for m_Color[ 1 ]
+long double Polygon::Green() //{{{
+{
+	return m_Color[ 1 ];
+}
+void Polygon::Green( long double nGreen )
+{
+	m_Color[ 1 ] = nGreen;
+} //}}}
 
-		// {G,S} for m_Color[ 1 ]
-		long double Green() //{{{
-		{
-			return m_Color[ 1 ];
-		}
-		void Green( long double nGreen )
-		{
-			m_Color[ 1 ] = nGreen;
-		} //}}}
+// {G,S} for m_Color[ 2 ]
+long double Polygon::Blue() //{{{
+{
+	return m_Color[ 2 ];
+}
+void Polygon::Blue( long double nBlue )
+{
+	m_Color[ 2 ] = nBlue;
+} //}}}
 
-		// {G,S} for m_Color[ 2 ]
-		long double Blue() //{{{
-		{
-			return m_Color[ 2 ];
-		}
-		void Blue( long double nBlue )
-		{
-			m_Color[ 2 ] = nBlue;
-		} //}}}
-
-		// {G,S} for m_Color[ 3 ]
-		long double Alpha() //{{{
-		{
-			return m_Color[ 3 ];
-		}
-		void Alpha( long double nAlpha )
-		{
-			m_Color[ 3 ] = nAlpha;
-		} //}}}
-
-	private:
-		long double m_Radius;
-		long double m_Color[ 4 ];
-		unsigned int m_NumSegments;
-
-};
+// {G,S} for m_Color[ 3 ]
+long double Polygon::Alpha() //{{{
+{
+	return m_Color[ 3 ];
+}
+void Polygon::Alpha( long double nAlpha )
+{
+	m_Color[ 3 ] = nAlpha;
+} //}}}
 
 #endif // POLYGON_CPP
