7d4377ea48a8199b69ec2dfd062db7e989cce211
Jeff Chapman II <JAChapmanII@GMail.com>
2010-06-30 13:12:27 -0400

    Switched to impulse/mover method of buffering changes

--- src/particle.cpp
+++ src/particle.cpp
@@ -29,12 +29,10 @@ Particle::Particle() :
 	radius( 0.0 ),
 	speed( 0.0 ),
 	momentum( 0.0 ),
-	m_Position(),
-	m_Velocity(),
-	m_Acceleration(),
-	m_nextPosition(),
-	m_nextVelocity(),
-	m_nextAcceleration(),
+	position(),
+	velocity(),
+	movers(),
+	impulses(),
 	m_Entities(),
 	m_Forces()
 {
@@ -57,31 +55,29 @@ void Particle::Work( Particle* B, long double deltaTime )
 
 void Particle::Update( long double deltaTime )
 { //{{{
-	m_nextVelocity.x += m_nextAcceleration.x * deltaTime;
-	m_nextVelocity.y += m_nextAcceleration.y * deltaTime;
+	for( std::vector< Vector< long double > >::iterator i = this->movers.begin();
+			i != this->movers.end(); i++ )
+	{
+		this->position += (*i);
+	}
+	this->movers.clear();
 
-	m_nextPosition.x += m_nextVelocity.x * deltaTime;
-	m_nextPosition.y += m_nextVelocity.y * deltaTime;
+	for( std::vector< Vector< long double > >::iterator i = this->impulses.begin();
+			i != this->impulses.end(); i++ )
+	{
+		this->velocity += (*i) * deltaTime;
+	}
+	this->impulses.clear();
 
-	m_nextAcceleration.x = 0.0;
-	m_nextAcceleration.y = 0.0;
+	this->position += this->velocity * deltaTime;
 
-	m_Acceleration.x = m_nextAcceleration.x;
-	m_Acceleration.y = m_nextAcceleration.y;
-
-	m_Velocity.x = m_nextVelocity.x;
-	m_Velocity.y = m_nextVelocity.y;
-
-	m_Position.x = m_nextPosition.x;
-	m_Position.y = m_nextPosition.y;
-
-	this->speed = sqrt( XVelocity()*XVelocity() + YVelocity()*YVelocity() );
+	this->speed = this->velocity.Magnitude();
 	this->momentum = this->mass * this->speed;
 
-	for( std::vector< Entity* >::iterator i = m_Entities.begin(); i != m_Entities.end(); ++i )
+	for( std::vector< Entity* >::iterator i = this->m_Entities.begin();
+			i != this->m_Entities.end(); ++i )
 	{
-		(*i)->position.x = XPosition();
-		(*i)->position.y = YPosition();
+		(*i)->position = this->position;
 	}
 
 } //}}}
@@ -119,206 +115,5 @@ Entity* Particle::GetEntity()
 	return NULL;
 	// TODO Error
 } //}}}
-
-// Position stuff
-//{{{
-// {G,S}etter for current XPosition
-long double Particle::XPosition() //{{{
-{
-	return m_Position.x;
-}
-void Particle::CurrentXPosition( long double nXPosition )
-{
-	m_Position.x = nXPosition;
-	m_nextPosition.x = nXPosition;
-	for( std::vector< Entity* >::iterator i = m_Entities.begin(); i != m_Entities.end(); ++i )
-	{
-		(*i)->position.x = nXPosition;
-	}
-} //}}}
-
-// {G,S}etter for current YPosition
-long double Particle::YPosition() //{{{
-{
-	return m_Position.y;
-}
-void Particle::CurrentYPosition( long double nYPosition )
-{
-	m_Position.y = nYPosition;
-	m_nextPosition.y = nYPosition;
-	for( std::vector< Entity* >::iterator i = m_Entities.begin(); i != m_Entities.end(); i++ )
-	{
-		(*i)->position.y = nYPosition;
-	}
-} //}}}
-
-// Setter for both current Positions
-void Particle::CurrentPositions( long double nXPosition, long double nYPosition) //{{{
-{
-	m_Position.x = nXPosition;
-	m_Position.y = nYPosition;
-	m_nextPosition.x = nXPosition;
-	m_nextPosition.y = nYPosition;
-	for( std::vector< Entity* >::iterator i = m_Entities.begin(); i != m_Entities.end(); i++ )
-	{
-		(*i)->position.x = nXPosition;
-		(*i)->position.y = nYPosition;
-	}
-} //}}}
-
-
-// {G,S}etter for next XPosition
-long double Particle::NextXPosition() //{{{
-{
-	return m_nextPosition.x;
-}
-void Particle::XPosition( long double nXPosition )
-{
-	m_nextPosition.x = nXPosition;
-} //}}}
-
-// {G,S}etter for next YPosition
-long double Particle::NextYPosition() //{{{
-{
-	return m_nextPosition.y;
-}
-void Particle::YPosition( long double nYPosition )
-{
-	m_nextPosition.y = nYPosition;
-} //}}}
-
-// Setter for both next Positions
-void Particle::Positions( long double nXPosition, long double nYPosition ) //{{{
-{
-	XPosition( nXPosition );
-	YPosition( nYPosition );
-} //}}}
-//}}}
-
-// Velocity stuff
-//{{{
-// {G,S}etter for current XVelocity
-long double Particle::XVelocity() //{{{
-{
-	return m_Velocity.x;
-}
-void Particle::CurrentXVelocity( long double nXVelocity )
-{
-	m_Velocity.x = nXVelocity;
-	this->speed = sqrt( nXVelocity*nXVelocity + YVelocity()*YVelocity() );
-} //}}}
-
-// {G,S}etter for current YVelocity
-long double Particle::YVelocity() //{{{
-{
-	return m_Velocity.y;
-}
-void Particle::CurrentYVelocity( long double nYVelocity )
-{
-	m_Velocity.y = nYVelocity;
-	this->speed = sqrt( XVelocity()*XVelocity() + nYVelocity*nYVelocity );
-} //}}}
-
-// Setter for both current Velocitys
-void Particle::CurrentVelocities( long double nXVelocity, long double nYVelocity) //{{{
-{
-	m_Velocity.x = nXVelocity;
-	m_Velocity.y = nYVelocity;
-	this->speed = sqrt( nXVelocity*nXVelocity + nYVelocity*nYVelocity );
-} //}}}
-
-
-// {G,S}etter for next XVelocity
-long double Particle::NextXVelocity() //{{{
-{
-	return m_nextVelocity.x;
-}
-void Particle::XVelocity( long double nXVelocity )
-{
-	m_nextVelocity.x = nXVelocity;
-} //}}}
-
-// {G,S}etter for next YVelocity
-long double Particle::NextYVelocity() //{{{
-{
-	return m_nextVelocity.y;
-}
-void Particle::YVelocity( long double nYVelocity )
-{
-	m_nextVelocity.y = nYVelocity;
-} //}}}
-
-// Setter for both next Velocitys
-void Particle::Velocities( long double nXVelocity, long double nYVelocity ) //{{{
-{
-	XVelocity( nXVelocity );
-	YVelocity( nYVelocity );
-} //}}}
-
-// Setter for both next Velocitys
-void Particle::Velocities( Vector< long double > nVelocity ) //{{{
-{
-	XVelocity( nVelocity.x );
-	YVelocity( nVelocity.y );
-} //}}}
-//}}}
-
-// Acceleration stuff
-//{{{
-// {G,S}etter for current XAcceleration
-long double Particle::XAcceleration() //{{{
-{
-	return m_Acceleration.x;
-}
-void Particle::CurrentXAcceleration( long double nXAcceleration )
-{
-	m_Acceleration.x = nXAcceleration;
-} //}}}
-
-// {G,S}etter for current YAcceleration
-long double Particle::YAcceleration() //{{{
-{
-	return m_Acceleration.y;
-}
-void Particle::CurrentYAcceleration( long double nYAcceleration )
-{
-	m_Acceleration.y = nYAcceleration;
-} //}}}
-
-// Setter for both current Accelerations
-void Particle::CurrentAccelerations( long double nXAcceleration, long double nYAcceleration) //{{{
-{
-	m_Acceleration.x = nXAcceleration;
-	m_Acceleration.y = nYAcceleration;
-} //}}}
-
-
-// {G,S}etter for next XAcceleration
-long double Particle::NextXAcceleration() //{{{
-{
-	return m_nextAcceleration.x;
-}
-void Particle::XAcceleration( long double nXAcceleration )
-{
-	m_nextAcceleration.x = nXAcceleration;
-} //}}}
-
-// {G,S}etter for next YAcceleration
-long double Particle::NextYAcceleration() //{{{
-{
-	return m_nextAcceleration.y;
-}
-void Particle::YAcceleration( long double nYAcceleration )
-{
-	m_nextAcceleration.y = nYAcceleration;
-} //}}}
-
-// Setter for both next Accelerations
-void Particle::Accelerations( long double nXAcceleration, long double nYAcceleration ) //{{{
-{
-	XAcceleration( nXAcceleration );
-	YAcceleration( nYAcceleration );
-} //}}}
-//}}}
 
 #endif // "PARTICLE_CPP"
--- src/particle.hpp
+++ src/particle.hpp
@@ -50,89 +50,6 @@ class Particle
 		/// Return first entity in m_Entities or null if there are none.
 		Entity* GetEntity();
 
-		/// Getter for current XPosition
-		inline long double XPosition();
-		/// Setter for current XPosition
-		inline void CurrentXPosition( long double nXPosition );
-
-		/// Getter for current YPosition
-		inline long double YPosition();
-		/// Setter for current YPosition
-		inline void CurrentYPosition( long double nYPosition );
-
-		/// Setter for both current Positions
-		inline void CurrentPositions( long double nXPosition, long double nYPosition);
-
-		/// Getter for next XPosition
-		inline long double NextXPosition();
-		/// Setter for next XPosition
-		inline void XPosition( long double nXPosition );
-
-		/// Getter for next YPosition
-		inline long double NextYPosition();
-		/// Setter for next YPosition
-		inline void YPosition( long double nYPosition );
-
-		/// Setter for both next Positions
-		inline void Positions( long double nXPosition, long double nYPosition );
-
-
-		/// Getter for current XVelocity
-		inline long double XVelocity();
-		/// Setter for current XVelocity
-		inline void CurrentXVelocity( long double nXVelocity );
-
-		/// Getter for current YVelocity
-		inline long double YVelocity();
-		/// Setter for current YVelocity
-		inline void CurrentYVelocity( long double nYVelocity );
-
-		/// Setter for both current Velocitys
-		inline void CurrentVelocities( long double nXVelocity, long double nYVelocity);
-
-		/// Getter for next XVelocity
-		inline long double NextXVelocity();
-		/// Setter for next XVelocity
-		inline void XVelocity( long double nXVelocity );
-
-		/// Getter for next YVelocity
-		inline long double NextYVelocity();
-		/// Setter for next YVelocity
-		inline void YVelocity( long double nYVelocity );
-
-		/// Setter for both next Velocitys
-		inline void Velocities( long double nXVelocity, long double nYVelocity );
-
-		/// Setter for both next Velocitys
-		inline void Velocities( Vector< long double > nVelocity );
-
-
-		/// Getter for current XAcceleration
-		inline long double XAcceleration();
-		/// Setter for current XAcceleration
-		inline void CurrentXAcceleration( long double nXAcceleration );
-
-		/// Getter for current YAcceleration
-		inline long double YAcceleration();
-		/// Setter for current YAcceleration
-		inline void CurrentYAcceleration( long double nYAcceleration );
-
-		/// Setter for both current Accelerations
-		inline void CurrentAccelerations( long double nXAcceleration, long double nYAcceleration);
-
-		/// Getter for next XAcceleration
-		inline long double NextXAcceleration();
-		/// Setter for next XAcceleration
-		inline void XAcceleration( long double nXAcceleration );
-
-		/// Getter for next YAcceleration
-		inline long double NextYAcceleration();
-		/// Setter for next YAcceleration
-		inline void YAcceleration( long double nYAcceleration );
-
-		/// Setter for both next Accelerations
-		inline void Accelerations( long double nXAcceleration, long double nYAcceleration );
-
 		/// Mass of particle
 		long double mass;
 		/// Radius -- used for collisions which is bad, need more generic collision system
@@ -142,22 +59,15 @@ class Particle
 		/// this->speed*this->mass, momentum of particle
 		long double momentum;
 
+		Vector< long double > position;
+		Vector< long double > velocity;
+
+		/// Movers (changes in position) that should affect particle
+		std::vector< Vector< long double > > movers;
+		/// Impulses (changes in velocity) that should act on particle
+		std::vector< Vector< long double > > impulses;
+
 	private:
-
-		/// Vector representing the current position of the particle
-		Vector< long double > m_Position;
-		/// Vector representing the current velocity of the particle
-		Vector< long double > m_Velocity;
-		/// Vector representing the current acceleration of the particle
-		Vector< long double > m_Acceleration;
-
-		/// Vector representing the next position of the particle
-		Vector< long double > m_nextPosition;
-		/// Vector representing the next velocity of the particle
-		Vector< long double > m_nextVelocity;
-		/// Vector representing the next acceleration of the particle
-		Vector< long double > m_nextAcceleration;
-
 		/// std::vector of Entity's representincg this object on the screen
 		std::vector< Entity* > m_Entities;
 		/// std::vector of Forces this particle "emits"
